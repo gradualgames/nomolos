a9a656c590ae6197e7fc3716d944a14a206c96ff
Derek Andrews <Derek Andrews@d887df96-dd9d-fa4b-9048-eb4ea8a40bc6>
2010-01-10 01:41:18 +0000

    placing the reset scroll code for the level in state after the vblank wait, where it ought to be. removing scroll setting code from level in PPU update, since we aren't changing it in real time.
    git-svn-id: svn://localhost/Nomolos/trunk@232 d887df96-dd9d-fa4b-9048-eb4ea8a40bc6

--- levelInState.asm
+++ levelInState.asm
@@ -138,16 +138,18 @@ levelInStateRun:
   sta w0+1
   jsr displayString
   
-  lda #0
-  sta $2005
-  sta $2005
-  
+ 
   ;turn sprite and background visibility on
   lda #( ( 1 << PPU0_EXECUTE_NMI ) | ( 1 << PPU0_ADDRESS_INCREMENT ) | ( 1 << PPU0_SPRITE_PATTERN_TABLE_ADDRESS ) )
   sta $2000
   
   ;wait for vblank so when we turn graphics back on we don't get ugly scrambling =)
   waitVBlank
+  
+  ;reset scroll
+  lda #0
+  sta $2005
+  sta $2005
   
   lda #( ( 1 << PPU1_SPRITE_VISIBILITY ) | ( 1 << PPU1_BACKGROUND_VISIBILITY ) | ( 1 << PPU1_BACKGROUND_CLIPPING ) | ( 1 << PPU1_SPRITE_CLIPPING ) )
   sta $2001
@@ -164,9 +166,5 @@ stateCommandComplete:
   jmp updateFinished
 
 levelInPPUUpdate:
-
-  ;lda #0
-  ;sta $2005
-  ;sta $2005
 
   jmp updatePPUFinished
