5783e32f367bb789bfc2f905dd2897912c60d2e9
Derek Andrews <Derek Andrews@d887df96-dd9d-fa4b-9048-eb4ea8a40bc6>
2010-05-08 21:58:50 +0000

    Removing all binary data, left over from Famitracker.
    git-svn-id: svn://localhost/Nomolos/trunk@405 d887df96-dd9d-fa4b-9048-eb4ea8a40bc6

--- src/FixedBankData.asm
+++ src/FixedBankData.asm
@@ -314,10 +314,6 @@ getItemSound:
 banktable:
   .byte $00, $01, $02, $03, $04, $05, $06, $07
 
-.export haltmusic
-haltmusic:
-.incbin "data/haltmusic.bin"
-
 .export title_music
 title_music:
   .word k13_square1
