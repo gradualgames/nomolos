0b3726b3d1449f4c90c55b903be96a56b90583f7
Derek Andrews <Derek Andrews@d887df96-dd9d-fa4b-9048-eb4ea8a40bc6>
2011-01-29 04:13:34 +0000

    Changing all symbols in the ram module to use .global instead of .export and .import
    git-svn-id: svn://localhost/Nomolos/trunk@620 d887df96-dd9d-fa4b-9048-eb4ea8a40bc6

--- src/ram.asm
+++ src/ram.asm
@@ -1,16 +1,12 @@
+.include "ram.inc"
+
 .segment "STACK"
-.export stack
 stack:  .res 256
 
 .segment "BSS"
-.export sprite
 sprite: .res 256
 
-.export entity_instances
 entity_instances: .res 256
-.export entity_locals
 entity_locals: .res 256
-.export entity_counters
 entity_counters: .res 32
-.export dynamic_palette
 dynamic_palette: .res 32
