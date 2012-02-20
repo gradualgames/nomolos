17513433b0ba386432966061dd07451be68b47bf
Derek Andrews <Derek Andrews@d887df96-dd9d-fa4b-9048-eb4ea8a40bc6>
2012-02-20 04:28:02 +0000

    Adding yet another iceball to Nogard's attack to make it impossible to stand safely in front of him. Moving Nogard over one tile so there's less room to jump. This forces the attacks of Nogard to be more confrontational and Nomolos must carefully jump around the iceballs to avoid getting hit. Stalactites fall faster now. One of the iceballs moves vertically, and I had to add some logic to ensure it dies offscreen in this case. Also updating the hitbox script for new address of nomolos_is_deadly.
    git-svn-id: svn://localhost/Nomolos/trunk@1040 d887df96-dd9d-fa4b-9048-eb4ea8a40bc6

--- src/boss1.asm
+++ src/boss1.asm
@@ -158,14 +158,13 @@ boss1_meta_tile_column_table = MetaTileColumnTable
 boss1_meta_tile_table = MetaTileTable
 
 Map:
-  .byte $00,$00,$00,$01,$02,$03,$04,$05,$06,$00,$00,$00,$00,$00,$00,$00
+  .byte $00,$00,$00,$01,$02,$03,$04,$00,$05,$00,$00,$00,$00,$00,$00,$00
 MapColumnTable:
   .byte $00,$00,$00,$00
-  .byte $00,$00,$01,$00
-  .byte $00,$02,$03,$00
-  .byte $00,$04,$05,$00
-  .byte $00,$06,$07,$00
-  .byte $00,$08,$00,$00
+  .byte $00,$01,$02,$00
+  .byte $00,$03,$04,$00
+  .byte $00,$05,$06,$00
+  .byte $00,$07,$08,$00
   .byte $00,$09,$00,$00
 AttributeColumnTable:
   .byte $00,$00,$00,$00,$00,$00,$50,$00
